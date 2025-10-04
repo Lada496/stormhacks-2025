bf51df653730416af91bc6106c66e81b452bd124
Lada496 <67321065+Lada496@users.noreply.github.com>
2025-10-04 14:40:38 -0700

    updated README
    test commit
    
    Sam's commits:  Added file structure, assets and required components

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,6 +5,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <ol>
+          <li>
+            Get started by editing da code in <code>src/app/page.js</code>.
+          </li>
+          <li>Save da project and see your changes instantly.</li>
+        </ol>
+
         <Header></Header>
       </main>
     </div>
