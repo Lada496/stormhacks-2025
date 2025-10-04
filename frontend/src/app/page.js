7b70de9875b328f202eb0d800b881d5a59f481ce
samlui1009 <sam_lui@hotmail.ca>
2025-10-04 16:37:09 -0700

    Stylized footer completed

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import Header from "./components/Header"; // Adjust the path based on your folder structure
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Header></Header>
+        <Footer></Footer>
       </main>
     </div>
   );
