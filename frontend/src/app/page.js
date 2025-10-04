9b8fa910471d5b398d48a6e42a6296d4ad873971
samlui1009 <sam_lui@hotmail.ca>
2025-10-04 16:22:57 -0700

    Stylized header complete

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,11 @@
 import styles from "./page.module.css";
+import Header from "./components/Header"; // Adjust the path based on your folder structure
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
+        <Header></Header>
       </main>
     </div>
   );
