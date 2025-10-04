9fddd99b2b2892aa519623055f0ade817cd9d45a
samlui1009 <sam_lui@hotmail.ca>
2025-10-04 16:22:57 -0700

    Implemented front page elements with placeholders

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,16 +1,11 @@
 import styles from "./page.module.css";
+import Header from "./components/Header"; // Adjust the path based on your folder structure
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <ol>
-          <li>
-            Get started by editing da code in <code>src/app/page.js</code>.
-          </li>
-          <li>Save da project and see your changes instantly.</li>
-        </ol>
-
+        <Header></Header>
       </main>
     </div>
   );
